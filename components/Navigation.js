afabd89f44a57da021dab612dff9f360bb34dda0
Daniel Rotter <daniel.rotter@gmail.com>
2017-04-19 22:09:43 +0200

    added toggling navigation list

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,43 +2,102 @@ import React from 'react';
 import styled from 'styled-components';
 import Button from './Button';
 
-const logoWidth = 100;
-const logoHeight = 100;
-const togglerSize = 60;
+const logoSize = 80;
+const togglerSize = 40;
+const navigationTop = logoSize + togglerSize + 50;
 
-const NavigationContainer = styled.nav`
+const HeaderContainer = styled.nav`
     position: fixed;
     top: 20px;
     left: 50%;
-    width: ${logoWidth}px;
-    margin: 0 0 0 ${-logoWidth / 2}px;
+    z-index: 2;
+    width: ${logoSize}px;
+    margin: 0 0 0 ${-logoSize / 2}px;
 `;
 
 const Logo = styled.div`
-    position: absolute;
-    height: ${logoHeight}px;
-    width: ${logoWidth}px;
+    position: relative;
+    height: ${logoSize}px;
+    width: ${logoSize}px;
     z-index: 2;
 
     background: url(static/logo.png);
-    background-size: 100px 100px;
+    background-size: ${logoSize}px ${logoSize}px;
+    border-radius: ${logoSize / 2}px;
+    box-shadow: 0 2px 30px 0 rgba(0, 0, 0, .2);
 `;
 
 const Toggler = styled(Button)`
-    position: absolute;
-    top: 90px;
+    position: relative;
+    top: -5px;
     left: 50%;
     z-index: 1;
     margin: 0 0 0 ${-togglerSize / 2}px;
+
+    box-shadow: 0 2px 30px 0 rgba(0, 0, 0, .2);
+`;
+
+const List = styled.ul`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    padding: ${navigationTop}px 0 0 0;
+
+    background-color: #fff;
+
+    list-style-type: none;
+    list-style-position: inside;
+
+    text-align: center;
+    text-transform: uppercase;
+    font-family: Teko;
+    font-size: 40px;
+    color: #2C2C2C;
+    line-height: 44px;
+
+    > li {
+        margin: 20px 0 0 0;
+
+        letter-spacing: 1px;
+    }
 `;
 
 export default class Navigation extends React.Component {
+    state = {
+        opened: false,
+    };
+
     render() {
+        const navigationList = this.state.opened ? this.renderNavigationList() : '';
+
         return (
-            <NavigationContainer>
-                <Logo />
-                <Toggler size={togglerSize} />
-            </NavigationContainer>
+            <header>
+                <HeaderContainer opened={this.state.opened}>
+                    <Logo />
+                    <Toggler size={togglerSize} onClick={this.handleToggle} />
+                </HeaderContainer>
+                {navigationList}
+            </header>
         );
     }
+
+    renderNavigationList() {
+        return (
+            <List>
+                <li>Schedule</li>
+                <li>Venue</li>
+                <li>About</li>
+                <li>Blog</li>
+                <li>AgentConf 2017</li>
+            </List>
+        );
+    }
+
+    handleToggle = () => {
+        this.setState((prevState) => ({
+            opened: !prevState.opened,
+        }));
+    };
 }
--- components/Page.js
+++ components/Page.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {injectGlobal} from 'styled-components';
+import Head from 'next/head';
 import Navigation from './Navigation';
 
 injectGlobal`
-    body {
+    * {
         margin: 0;
         padding: 0;
+    }
 
+    body {
         background: #aaa;
     }
 `;
@@ -20,6 +23,9 @@ export default class Page extends React.Component {
     render() {
         return (
             <div>
+                <Head>
+                    <link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet" />
+                </Head>
                 <Navigation />
                 {this.props.children}
             </div>
