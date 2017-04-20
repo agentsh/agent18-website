c0b2947051603baa19fdbc285dacbd3db46e6f67
Daniel Rotter <daniel.rotter@gmail.com>
2017-04-20 19:55:36 +0200

    center the navigation vertically

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,15 +4,14 @@ import Button from './Button';
 
 const logoSize = 80;
 const togglerSize = 40;
-const navigationTop = logoSize + togglerSize + 50;
 
-const HeaderContainer = styled.nav`
+const HeaderContainer = styled.div`
     position: fixed;
     top: 20px;
     left: 50%;
     z-index: 2;
     width: ${logoSize}px;
-    margin: 0 0 0 ${-logoSize / 2}px;
+    transform: translateX(-50%);
 `;
 
 const Logo = styled.div`
@@ -32,24 +31,19 @@ const Toggler = styled(Button)`
     top: -5px;
     left: 50%;
     z-index: 1;
-    margin: 0 0 0 ${-togglerSize / 2}px;
+    transform: translateX(-50%);
 
     box-shadow: 0 2px 30px 0 rgba(0, 0, 0, .2);
 `;
 
-const List = styled.ul`
-    position: absolute;
+const List = styled.nav`
+    position: fixed;
     top: 0;
     right: 0;
     bottom: 0;
     left: 0;
-    padding: ${navigationTop}px 0 0 0;
 
     background: url(static/menu-background.png) #fff;
-    background-size: 80px;
-
-    list-style-type: none;
-    list-style-position: inside;
 
     text-align: center;
     text-transform: uppercase;
@@ -58,10 +52,20 @@ const List = styled.ul`
     color: #2C2C2C;
     line-height: 44px;
 
-    > li {
-        margin: 20px 0 0 0;
+    > ul {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
 
-        letter-spacing: 1px;
+        list-style-type: none;
+        list-style-position: inside;
+
+        > li {
+            margin: 20px 0 0 0;
+
+            letter-spacing: 1px;
+        }
     }
 `;
 
@@ -87,11 +91,13 @@ export default class Navigation extends React.Component {
     renderNavigationList() {
         return (
             <List>
-                <li>Schedule</li>
-                <li>Venue</li>
-                <li>About</li>
-                <li>Blog</li>
-                <li>AgentConf 2017</li>
+                <ul>
+                    <li>Schedule</li>
+                    <li>Venue</li>
+                    <li>About</li>
+                    <li>Blog</li>
+                    <li>AgentConf 2017</li>
+                </ul>
             </List>
         );
     }
