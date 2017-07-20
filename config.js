cc8c78ed0745676cad5a83eacb3cd67ff5c78565
Thomas Vogel <toms.vogel@gmail.com>
2017-07-20 19:29:36 +0200

    previous solution for enviroment did not work on next reload (#19)
    * previous solution for enviroment did not work on next reload
    
    * formating config

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 const config = {
-    baseUrl: 'http://127.0.0.1:8001',
+    baseUrl: process.env.API_URL,
 };
-
-if (process.env.NODE_ENV === 'prod') {
-    config.baseUrl = 'http://api.agent.sh';
-}
 
 export default config;
