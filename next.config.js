b583cd3d7598369664b5026c345266f80781f4bc
Thomas Vogel <thomas@arkulpa.at>
2017-07-20 19:28:15 +0200

    formating config

--- next.config.js
+++ next.config.js
@@ -4,13 +4,11 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    webpack: (config) => {
+    webpack: config => {
         config.plugins = config.plugins || [];
-
         config.plugins = [
             ...config.plugins,
-
-      // Read the .env file
+            // Read the .env file
             new Dotenv({
                 path: path.join(__dirname, '.env'),
                 systemvars: true,
@@ -19,4 +17,4 @@ module.exports = {
 
         return config;
     },
-};                                         
+};
