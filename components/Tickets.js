00294f60301bb6b3741d74a39312fdee4e1a2319
Thomas Vogel <thomas@arkulpa.at>
2017-07-20 19:20:04 +0200

    added booking url

--- components/Tickets.js
+++ components/Tickets.js
@@ -1,46 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
-
+const bookingUrl = 'https://www.eventbrite.de/e/agentconf-2018-tickets-31262914218?ref=elink';
 const TicketWrapper = styled.div`
     position: absolute;
     z-index: 2;
-    width:1060px;
-    max-width:100%;
-    left:0;
-    right:0;
+    width: 1060px;
+    max-width: 100%;
+    left: 0;
+    right: 0;
     margin: 0 auto;
     height: 500px;
-    margin-bottom:400px;
+    margin-bottom: 400px;
 `;
 
 const Title = styled.div`
-    width:150px;
-    font-size:48px;
-    line-height:54px;
-    float:right;
-    color: white; 
+    width: 150px;
+    font-size: 48px;
+    line-height: 54px;
+    float: right;
+    color: white;
     margin-bottom: 50px;
     margin-top: 200px;
-    text-transform: uppercase;   
+    text-transform: uppercase;
     font-family: Teko;
 `;
 
 const TicketBox = styled.div`
-    width:338px;
-    text-align:center;
-    float:left;
+    width: 338px;
+    text-align: center;
+    float: left;
     padding: 30px 20px;
     box-sizing: border-box;
     background: #282828;
     color: white;
-    
+
     hr {
-    display: block;
-    height: 1px;
-    border: 0;
-    border-top: 1px solid #757575;
-    margin: 1em 0;
-    padding: 0; 
+        display: block;
+        height: 1px;
+        border: 0;
+        border-top: 1px solid #757575;
+        margin: 1em 0;
+        padding: 0;
     }
 `;
 
@@ -77,18 +77,18 @@ const Description = styled.div`
 `;
 
 const ButtonLink = styled.div`
-    display:block;
-    height:40px;
+    display: block;
+    height: 40px;
     border-radius: 100px;
-    background-color: #231F20;
-    color:white;
-    text-align:center;
-    width:260px;
-    font-family: Teko;	
-    font-size: 14px;	
-    font-weight: 500;	
-    letter-spacing: 2px;	
-    line-height: 40px;	
+    background-color: #231f20;
+    color: white;
+    text-align: center;
+    width: 260px;
+    font-family: Teko;
+    font-size: 14px;
+    font-weight: 500;
+    letter-spacing: 2px;
+    line-height: 40px;
     text-align: center;
     text-transform: uppercase;
     margin: 0 auto;
@@ -107,9 +107,9 @@ export default class Tickets extends React.PureComponent {
                         <Price> 99€ </Price>
                         <hr />
                         <Description>
-              Conference 20.01 & 21.01<br />
-              All Talks <br />
-              Conference refreshments<br />
+                            Conference 20.01 & 21.01<br />
+                            All Talks <br />
+                            Conference refreshments<br />
                         </Description>
                         <ButtonLink>sold out</ButtonLink>
                     </ClosedTicketbox>
@@ -119,14 +119,12 @@ export default class Tickets extends React.PureComponent {
                         <Price> 199€</Price>
                         <hr />
                         <Description>
-              Conference 20.01 & 21.01<br />
-              All Talks <br />
-              Conference refreshments<br />
-              Networking Events
-
-            </Description>
+                            Conference 20.01 & 21.01<br />
+                            All Talks <br />
+                            Conference refreshments<br />
+                            Networking Events
+                        </Description>
                         <ButtonLink>buy now</ButtonLink>
-
                     </ActiveTicketbox>
                     <TicketBox>
                         <Headline>Ski addon</Headline>
@@ -134,8 +132,8 @@ export default class Tickets extends React.PureComponent {
                         <Price> 400€</Price>
                         <hr />
                         <Description>
-              2 Nights Hostel in Lech<br />
-              2 Ski passes in Lech<br />
+                            2 Nights Hostel in Lech<br />
+                            2 Ski passes in Lech<br />
                             <br />
                         </Description>
                         <ButtonLink>buy now</ButtonLink>
